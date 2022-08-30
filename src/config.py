13816b4387c7d9722b297ca1630dd8c437025ba0
godhw <shw0210@naver.com>
2022-08-30 17:19:58 +0900

    delete endpoint url

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ from pydantic import BaseSettings, HttpUrl
 
 
 class LLMSettings(BaseSettings):
-    llm_endpoint: HttpUrl = "https://bloom-176b.jp.ngrok.io"
-    llm_max_length: int = 2048
+    llm_endpoint: HttpUrl
+    llm_max_length: int
     # add more
 
 
